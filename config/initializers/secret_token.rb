d7b86748ff0c6688796df68511efc7acf18d0b0b
colorsocks <babkin@colorsocks.ru>
2014-09-07 12:51:17 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Da::Application.config.secret_key_base = '62690d9bd8bc5d29b2a4e78432ae776ce9b6793dfce183c01f840cbc1e4520cb1eed8d94f4f1d30e0afc5a9f40d1f3a5796b23d11862deb5b388e8721c03f98a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Da::Application.config.session_store :cookie_store, key: '_da_session'
